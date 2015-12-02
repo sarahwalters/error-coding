0471db3f6a17cdfe1cc89aac33b5128184dd4e89
Sarah Walters <sarah.walters@students.olin.edu>
2015-12-02 11:26:56 -0500

    Initial structure

---
+++ node.py
@@ -0,0 +1,8 @@
+class Node:
+  def __init__(self, path_metric, historical_path):
+    self.path_metric = path_metric
+    self.historical_path = historical_path
+
+  def create_next_nodes(self):
+    next_nodes = []
+    return next_nodes
---
+++ trellis.py
@@ -0,0 +1,13 @@
+class Trellis:
+  def __init__(self, gen_polys, rcv_bits):
+    self.gen_polys = gen_polys # generator polynomials, e.g. ['111', '110']
+    self.k = len(gen_polys[0]) # window size, e.g. 3
+    self.rcv_bits = rcv_bits
+    self.lookup_table = {} # maps from state ID to possible next state IDs
+    self.col = None # will be a list of nodes
+
+    def create_next_col(self):
+      # calls create_next_nodes for all of the nodes in self.col
+      # then, groups all of the next_nodes by state id (eg '00') and keeps the one with the
+      #     smallest path metric from each group
+      pass

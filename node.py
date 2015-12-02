085ab3b1e607eac7d78bf3c0c75f553aa22329ea
Sarah Walters <sarah.walters@students.olin.edu>
2015-12-02 11:28:57 -0500

    Important comment

--- node.py
+++ node.py
@@ -1,7 +1,7 @@
 class Node:
   def __init__(self, path_metric, historical_path):
     self.path_metric = path_metric
-    self.historical_path = historical_path
+    self.historical_path = historical_path # newest to oldest
 
   def create_next_nodes(self):
     next_nodes = []

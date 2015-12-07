f8a65d010af31253d4246119e3366897103fda23
Sarah Walters <sarah.walters@students.olin.edu>
2015-12-07 15:38:40 -0500

    Accuracy vs. rate graph

--- encoder.py
+++ encoder.py
@@ -2,7 +2,7 @@ import random
 import string_utils
 
 class Encoder:
-  def __init__(self, gen_polys, mesg, err_rate):
+  def __init__(self, gen_polys, mesg, err_rate, print_flag=False):
     """
     gen_polynomials is a list a length k generator polynomials
     mesg is a string containing the bit message to transmit
@@ -14,6 +14,7 @@ class Encoder:
     self.r = len(gen_polys) #The number of parity bits made
     self.mesg = '0'*(self.k-1)+mesg
     self.encoded = self.__encode()
+    self.print_flag = print_flag
 
 
   def __encode(self):
@@ -39,13 +40,16 @@ class Encoder:
       if random.random()<=self.err_rate:
         noisy_msg[i]=noisy_msg[i]^1
         count+=1
-    print count, 'error(s) generated at', self.err_rate,'error rate.'
+
+    if self.print_flag:
+      print count, 'error(s) generated at', self.err_rate,'error rate.'
+
     return ''.join([str(x) for x in noisy_msg])
 
 if __name__ == '__main__':
-  #This particular test case from http://wits.ice.nsysu.edu.tw/course/pdfdownload/93_2/%E9%8C%AF%E8%AA%A4%E6%9B%B4%E6%AD%A3%E7%A2%BC/CC-ConvolutionalCode.pdf  
+  #This particular test case from http://wits.ice.nsysu.edu.tw/course/pdfdownload/93_2/%E9%8C%AF%E8%AA%A4%E6%9B%B4%E6%AD%A3%E7%A2%BC/CC-ConvolutionalCode.pdf
   polys =  ('1011','1111')
-  a = Encoder(polys,'10111',0.1)
+  a = Encoder(polys,'10111',0.1, print_flag=True)
   print "Input Message with 0 padding is", a.mesg
   print "Message encodes as", a.encoded
   print a.send(), "is noisy message"

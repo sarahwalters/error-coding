fbee28255feead4d8342e309432d3a0074ba934b
Kyle Flores <krm.flores@gmail.com>
2015-12-10 20:20:39 -0500

    code for window size graphics.

--- graphs.py
+++ graphs.py
@@ -1,6 +1,6 @@
 from test_bench import test
 import matplotlib.pyplot as plt
-
+import time
 
 def accuracy_vs_rate():
   # run simulations
@@ -32,5 +32,33 @@ def accuracy(polys, ber, num_iterations):
 
   return sum_error/float(num_iterations)
 
+def accuracy_vs_windowsz(max_window = 10):
+  # Larger window sizes obtained by repeating polys.
+  ber = 0.1
+  polys = ['11','10','01']
+  num_iterations = 10
+  accuracies = []
+  sizes = range(2,max_window+2)
+  for i in range(max_window):
+    print "Testing with window size ",i+2
+    now=time.time()
+    accuracies.append(accuracy(polys,ber,num_iterations))
+    print "Accuracy was ",accuracies[-1]
+    print "Test took ",time.time()-now,"seconds."
+    if len(polys[0]) < max_window:
+      for p_i in range(len(polys)):
+        polys[p_i]+=str(1-int(polys[p_i][-1]))
+
+  plt.plot(sizes,accuracies)
+  plt.title('Accuracy vs. Window Size, BER=%s' % ber)
+  plt.xlabel('Window Size, k')
+  plt.ylabel('Hamming distance over 1000 bits')
+  plt.show()
+  fig = plt.gcf()
+  fig.savefig('imgs/accuracy_vs_window_size.png')
+  return (sizes, accuracies)
+
+
 if __name__ == '__main__':
-  print accuracy_vs_rate()
+  #print accuracy_vs_rate()
+  accuracy_vs_windowsz()
